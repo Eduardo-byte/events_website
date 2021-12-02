10a095c4c2d047f0e5e141852efe70492f119076
Eduardo-byte <eduardo7.brito@gmail.com>
2021-12-02 19:59:24 +0000

    add user regist

--- myclub_website/settings.py
+++ myclub_website/settings.py
@@ -27,6 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'events',
     'debug_toolbar',
+    'members',
+    "django_static_fontawesome",
     
 ]
 
--- myclub_website/urls.py
+++ myclub_website/urls.py
@@ -5,5 +5,12 @@ import debug_toolbar
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('events.urls')),
+    path('members/', include('django.contrib.auth.urls')),
+    path('members/', include('members.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
 ]
+
+# Configure Admin titles
+admin.site.site_header = "My Club | Admin page"
+admin.site.site_title = "My Club"
+admin.site.index_title = "Admin Area"
